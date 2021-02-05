f943ff4d4be187f8c85270e7a982cdf87e7a5be2
zeeshan_dev <zeeshan@afnanilab.com>
2021-02-05 19:06:55 +0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
 });
+
+describe('App component', () => {
+  it('starts with a count of 0', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 0');
+  });
+
+  it('increments count by 1 when the increment button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.increment');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 1');
+  });
+
+  it('decrements count by 1 when the decrement button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const decrementBtn = wrapper.find('button.decrement');
+    decrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: -1');
+  });
+});
+
